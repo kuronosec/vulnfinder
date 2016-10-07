4a7e816e42d9a58695c03071a78fc99839a0d5b1
Camilo Sampedro <camilo.sampedro@udea.edu.co>
2016-10-07 12:47:48 -0500

    TOE reading from target.txt added

---
+++ report generator/src/main/resources/main.js
@@ -0,0 +1,26 @@
+function loadJQuery(){
+
+var waitForLoad = function () {
+    if (typeof jQuery != "undefined") {
+        console.log("jquery loaded..");
+        $(function() {
+            $(".expand").on("click", function() {
+                $(this).next().slideToggle(200);
+                $expand = $(this).find(">:first-child");
+
+                if ($expand.text() == "+") {
+                    $expand.text("-");
+                } else {
+                    $expand.text("+");
+                }
+            });
+        });
+    } else {
+        console.log("jquery not loaded..");
+        window.setTimeout(waitForLoad, 500);
+    }
+ };
+ window.setTimeout(waitForLoad, 500);
+}
+
+window.onload = loadJQuery;
